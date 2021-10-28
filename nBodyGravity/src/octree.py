672f8dc779d58189332af9cf7f42ef20daf03b08
snow259 <johann.abraham7@gmail.com>
2021-10-28 22:32:41 +0530

    Point insertion works without errors, tree depth tracking added

--- nBodyGravity/src/octree.py
+++ nBodyGravity/src/octree.py
@@ -1,6 +1,6 @@
 class octree:
 
-	def __init__(self, center, width, capacity):
+	def __init__(self, center, width, capacity, depth=0, maxDepth=10):
 		# Define position and size
 		self.center = center
 		self.width = width
@@ -12,23 +12,27 @@ class octree:
 		self.mass = 0
 		self.com = center
 
+		# Node depth
+		self.maxDepth = 10
+		self.depth = depth
+
 		# Contents of octants
-		# Right handed axis, x towards, y right, z upwards
-		# 0: +++ : fTopRight
-		# 1: ++- : fTopLeft
-		# 2: +-+ : fBotRight
-		# 3: +-- : fBotLeft
-		# 4: -++ : rTopRight
-		# 5: -+- : rTopLeft
-		# 6: --+ : rBotRight
-		# 7: --- : rBotLeft
+		# Right handed axis, x right, y upwards, z towards
+		# 0: +++ : right, top, front
+		# 1: ++- : right, top, rear
+		# 2: +-+ : right, bot, front
+		# 3: +-- : right, bot, rear
+		# 4: -++ : left, top, front
+		# 5: -+- : left, top, rear
+		# 6: --+ : left, bot, front
+		# 7: --- : left, bot, rear
 		# 8 nested lists, because initialising with [] * 8 will create 8 references
 		# to the same object in memory
 		self.octants = [[], [], [], [], [], [], [], []]
 		self.contained = [0] * 8
 
 	def insert(self, point):
-		# Find octant to insertn point, check octant for capicity
+		# Find octant to insert point, check octant for capacity
 		octantIndex = self.findOctant(point)
 
 		if self.contained[octantIndex] < self.capacity:
@@ -51,14 +55,22 @@ class octree:
 
 		# Create octree
 		newCenter, newWidth = self.findPartitionCenterWidth(octantIndex)
-		self.octants[octantIndex] = octree(newCenter, newWidth, self.capacity)
+
+		self.octants[octantIndex] = octree(
+			newCenter,
+			newWidth,
+			self.capacity,
+			self.depth + 1,
+			self.maxDepth
+		)
+
 		# Inserts all points into octree
 		for point in points:
 			self.octants[octantIndex].insert(point)
 
 	def findPartitionCenterWidth(self, octantIndex):
 		newWidth = self.width / 2
-		newCenter = self.center
+		newCenter = self.center.copy()
 
 		# Calculating position of center of new octree within the octant
 		# Refer to coordinate convention above
