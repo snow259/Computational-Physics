f7b9c51de478eff2ee4d2de47b14fc683d64fa14
snow259 <67005128+snow259@users.noreply.github.com>
2023-02-06 21:49:25 -0500

    Conf struct for Macroquad configured for window size and title

--- Double-Pendulums/src/main.rs
+++ Double-Pendulums/src/main.rs
@@ -33,7 +33,18 @@ impl Pendulum {
     }
 }
 
-#[macroquad::main("Double Pendulums")]
+fn window_conf() -> Conf {
+    Conf {
+        // .to_owned() converts &str to String
+        window_title: "Double Pendulums".to_owned(),
+        window_width: 1280,
+        window_height: 720,
+        // Fills remaining values with defaults
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
     let mut rng = thread_rng();
     let radius = 5.0;
